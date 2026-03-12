dadb0d18aead8c74ba322aa48e4a14d5af3ede62
maxonchickdev <timetotime828@gmail.com>
2026-03-12 19:16:01 +0200

    feat: add favicon and update routing base URL
    - Added a favicon.svg file for the application icon.
    - Updated index.html to include the favicon link.
    - Configured Vite to set the base URL for production and development environments.
    - Modified main.tsx to use the dynamic base URL in the BrowserRouter.

--- src/main.tsx
+++ src/main.tsx
@@ -9,9 +9,14 @@ if (rootElement == null) {
   throw new Error("Root element not found");
 }
 
+const BASE =
+  import.meta.env.BASE_URL === "/"
+    ? "/"
+    : import.meta.env.BASE_URL.replace(/\/$/, "");
+
 createRoot(rootElement).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={BASE}>
       <App />
     </BrowserRouter>
   </StrictMode>,
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import react from "@vitejs/plugin-react";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  base:
+    process.env.NODE_ENV === "production"
+      ? "/software-development-blog/"
+      : "/",
   plugins: [react()],
   resolve: {
     alias: {
