9ae0759ad98e21f623422b5f75ad5b8a66913f3d
maxonchickdev <timetotime828@gmail.com>
2026-05-19 15:47:34 +0300

    fix(root): set browser router basename to root for consistent routing
    - Set browser router basename to root for consistent routing.

--- src/main.tsx
+++ src/main.tsx
@@ -10,11 +10,9 @@ if (rootElement == null) {
 	throw new Error("Root element not found");
 }
 
-const BASE = import.meta.env.BASE_URL === "/" ? "/" : import.meta.env.BASE_URL.replace(/\/$/, "");
-
 createRoot(rootElement).render(
 	<StrictMode>
-		<BrowserRouter basename={BASE}>
+		<BrowserRouter basename={"/"}>
 			<App />
 		</BrowserRouter>
 	</StrictMode>,
